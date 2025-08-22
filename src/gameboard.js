bc559d8e1384ef9ea148e1fe71d222375e0ec067
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-22 23:44:50 +0200

    game board set

---
+++ src/gameboard.js
@@ -0,0 +1,60 @@
+// gameboard.js
+
+import Ship from "./ship";
+
+export class Cell{
+    constructor(row,column,shipPoint = null, hit = false){
+        // cell creation OK
+        this.row = row
+        this.column = column
+        this.shipPoint = shipPoint
+        this.hit = hit
+        
+    }
+}
+
+export default class Gameboard{
+    constructor(size = 10){
+        //constructor tested
+        this.board = []
+        for (let i = 0 ; i<size; i++){
+            let rowAdd =[];
+            for (let j=0; j<size;j++){
+                let columnAdd = new Cell(i,j)
+                rowAdd.push(columnAdd)
+            }
+            this.board.push(rowAdd)
+        }
+        this.ship = []
+    }
+    receiveAttack(row,column){
+        // tested. be carfeul Do not use .board but rather gameBoardTest2.receiveAttack(0,1)
+        if (this.board[row][column].shipPoint !== null){
+            
+            this.board[row][column].shipPoint.hit()
+        }
+        this.board[row][column].hit =true
+    }
+    addShip(newShip, coordinates){
+        for (const coordinate of coordinates){
+            let row = coordinate[0]
+            let column = coordinate[1]
+            if (this.board[row][column].shipPoint === null){
+                this.board[row][column].shipPoint = newShip
+            }
+        }
+        this.ship.push(newShip)
+    }
+    allSunk(){
+        let result = false
+        if (this.ship = []){
+            return true
+        }
+        for (const shipMember of this.ship){
+            if (shipMember.isSunk() === false){
+                return true
+            }
+            
+        }
+    }
+}
---
+++ src/gameboard.test.js
@@ -0,0 +1,82 @@
+import {Cell} from "./gameboard";
+import Ship from "./ship";
+import Gameboard from "./gameboard"
+
+
+
+
+test('test gameboard making', () => {
+    let gameBoardTest = new Gameboard(1)
+    console.log(gameBoardTest)
+    expect(gameBoardTest.board[0][0]).toEqual({ row : 0, column : 0, shipPoint : null, hit : false});
+});
+
+test('test 2 sized gameboard', () => {
+    let gameBoardTest2 = new Gameboard(2)
+    console.log(gameBoardTest2)
+    expect(gameBoardTest2.board).toEqual([
+        [{ row : 0, column : 0, shipPoint : null, hit : false},{ row : 0, column : 1, shipPoint : null, hit : false}],
+        [{ row : 1, column : 0, shipPoint : null, hit : false},{ row : 1, column : 1, shipPoint : null, hit : false}]
+    ]
+    );
+});
+
+test('test 2 sized gameboard and adding a ship', () => {
+    let gameBoardTest2 = new Gameboard(2)
+    let testShip = new Ship(1)
+    gameBoardTest2.board[0][0].shipPoint = testShip
+    expect(gameBoardTest2.board).toEqual([
+        [{ row : 0, column : 0, shipPoint : testShip, hit : false},{ row : 0, column : 1, shipPoint : null, hit : false}],
+        [{ row : 1, column : 0, shipPoint : null, hit : false},{ row : 1, column : 1, shipPoint : null, hit : false}]
+    ]
+    );
+});
+
+
+test('test hitting a ship', () => {
+    let gameBoardTest2 = new Gameboard(2)
+    let testShip = new Ship(1)
+    let testShip2 = new Ship(1)
+    gameBoardTest2.board[0][0].shipPoint = testShip
+    gameBoardTest2.board[0][1].shipPoint = testShip2
+    gameBoardTest2.receiveAttack(0,1)
+    expect(gameBoardTest2.board).toEqual([
+        [{ row : 0, column : 0, shipPoint : testShip, hit : false},{ row : 0, column : 1, shipPoint : testShip2, hit : true}],
+        [{ row : 1, column : 0, shipPoint : null, hit : false},{ row : 1, column : 1, shipPoint : null, hit : false}]
+    ]
+    );
+});
+
+test('test placing a ship', () => {
+    let gameBoardTest2 = new Gameboard(2)
+    let testShip = new Ship(1)
+    let testShip2 = new Ship(1)
+    let testShipcoord =[[0,0],[0,1]]
+    gameBoardTest2.addShip(testShip,testShipcoord)
+    expect(gameBoardTest2.board).toEqual([
+        [{ row : 0, column : 0, shipPoint : testShip, hit : false},{ row : 0, column : 1, shipPoint : testShip, hit : false}],
+        [{ row : 1, column : 0, shipPoint : null, hit : false},{ row : 1, column : 1, shipPoint : null, hit : false}]
+    ]
+    );
+});
+
+test('test allSunk', () => {
+    let gameBoardTest2 = new Gameboard(2)
+    let testShip = new Ship(1)
+    let testShip2 = new Ship(1)
+    let testShipcoord =[[0,0],[0,1]]
+    gameBoardTest2.addShip(testShip,testShipcoord)
+    gameBoardTest2.receiveAttack(0,1)
+    gameBoardTest2.receiveAttack(0,0)
+    expect(gameBoardTest2.board).toEqual([
+        [{ row : 0, column : 0, shipPoint : testShip, hit : true},{ row : 0, column : 1, shipPoint : testShip, hit : true}],
+        [{ row : 1, column : 0, shipPoint : null, hit : false},{ row : 1, column : 1, shipPoint : null, hit : false}]
+    ]
+    );
+    expect(gameBoardTest2.allSunk()).toBe(true);
+});
+
+
+
+
+
